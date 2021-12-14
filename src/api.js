4174226e37f424bd3cf47f7238f062adf2628488
PhilPinsdorf <philemon.pinsdorf@live.de>
2021-12-14 18:54:13 +0100

    .

--- src/api.js
+++ src/api.js
@@ -55,7 +55,7 @@ router.get('/newquestion', (req, res) => {
 
 // Connect to Database, then start express app
 var mongoDbUri = 'mongodb+srv://tos:IfhWYIlA70YTitpA@truthorshot.n259x.mongodb.net/Questions?retryWrites=true&w=majority'
-mongoose.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoDbUri, { useNewUrlParser: true })
 	.then((result) => {
 		console.log('Connected to Database!');
 		app.listen(3000, () => console.log('Listen to 3000!'));
