1a620b3c06f28d73fe5fa44fbd40bc813930daec
PhilPinsdorf <philemon.pinsdorf@live.de>
2021-12-14 18:45:11 +0100

    Moved Route back to File

--- src/api.js
+++ src/api.js
@@ -1,14 +1,59 @@
-//Imports
+// Imports
 const express = require('express');
 const mongoose = require('mongoose');
 const serverless = require('serverless-http')
-const router = require('./router.js')
 const cors = require('cors');
+const questionShema = require('./modules/questionShema');
 
-//Express Instance
+// Express Instance
 const app = express();
+const router = express.Router();
 
-//Connect to Database, then start express app
+// Route to new question
+router.get('/newquestion', (req, res) => {
+    var doc = questionShema[Math.floor(Math.random() * questionShema.length)]
+  
+    // Get the count of all questions
+    doc.count().exec(function (err, count) {
+        //Set Color and Title to corresponding Document
+        var color = ""
+        var title = ""
+    
+        switch (doc) {
+            case questionShema[0]:
+            color = "#FF5D4E";
+            title = "ICH HAB NOCH NIE";
+            break;
+            
+            case questionShema[1]:
+            color = "#85E8E0";
+            title = "WER WÜRDE EHER";
+            break;
+    
+            case questionShema[2]:
+            color = "#47D98B";
+            title = "WAHRHEIT";
+            break;
+        }
+  
+        // Get a random entry from the Document
+        var random = Math.floor(Math.random() * count)
+    
+        //Load all Questions from the Document but only get the one with the offset from our random number
+        doc.findOne().skip(random).exec((err, result) => {
+            if (err) throw err
+    
+            //Send Back Data from the Choosen entry
+            res.status(200).send({
+            text: result.text,
+            title: title,
+            color: color
+            })
+        })
+    })
+})
+
+// Connect to Database, then start express app
 var mongoDbUri = 'mongodb+srv://tos:IfhWYIlA70YTitpA@truthorshot.n259x.mongodb.net/Questions?retryWrites=true&w=majority'
 mongoose.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true })
 	.then((result) => {
@@ -17,7 +62,7 @@ mongoose.connect(mongoDbUri, { useNewUrlParser: true, useUnifiedTopology: true }
 		app.use(express.json());
 		app.use(cors());
 		app.use(express.static('public'))
-		app.use('/.netlify/functions/router', router)
+		app.use('/.netlify/functions/api', router)
 	})
 	.catch((err) => {
 		console.log(err);
